0fada7967878c162619b80d4f0ac0521b52a9ea9
Tofiyin <tofiyinkay80@gmail.com>
2022-09-14 02:13:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "main.h"
+#include <main.h>
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0
  */
-void print_alphabet(void)
+int main(void)
 {
- char c;
+         _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
 
- for (c='a';c<='z';c++)
-
-{
-
-_putchar(c);
-
-}
-	}
-_putchar('\n');
+   return (0);
 
 }
